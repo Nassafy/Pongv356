591db9fcd128bbc69cec8fee1123ff2244745edd
Nassafy <matthiaspinard@aol.com>
2018-01-24 19:20:43 +0100

    add asset

---
+++ Assets/Ball.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+    public float speed = 30;
+	// Use this for initialization
+	void Start () {
+        GetComponent<Rigidbody2D>().velocity = Vector2.right * speed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    float hitFactor(Vector2 ballPos, Vector2 racketPos, float racketHeigth)
+    {
+        return (ballPos.y - racketPos.y) / racketHeigth;
+    }
+
+    private void OnCollisionEnter2D(Collision2D coll)
+    {
+        if(coll.gameObject.name == "RacketLeft")
+        {
+
+        }
+    }
+}
