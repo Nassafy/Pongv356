0ce3f7bf4b851e3336da6cb2e32069129887e4e1
Nassafy <matthiaspinard@aol.com>
2018-01-24 19:20:26 +0100

    envoie to soce

---
+++ Assets/MoveRacket.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveRacket : MonoBehaviour {
+
+    public float speed = 30;
+    public string axis = "Vertical";
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        float v = Input.GetAxisRaw(axis);
+        GetComponent<Rigidbody2D>().velocity = new Vector2(0, v) * 30;
+	}
+}
